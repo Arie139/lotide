bfb95a7449c7a5b8cf1308ac318925fcae096a61
Ariel <see-har@hotmail.com>
2021-10-26 15:38:42 +0000

    updated

---
+++ head.js
@@ -0,0 +1,11 @@
+const assertEquals = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅✅✅Assertion Passed:${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    return console.log(`❌❌❌Assertion Failed:${actual} !== ${expected}`);
+  }
+};
+
+const head = function(someArray){
+  return console.log(someArray[0]);
+}
