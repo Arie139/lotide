392c2fa18e9975d06001579df864f989e9a5836b
Ariel <see-har@hotmail.com>
2021-10-26 14:20:19 +0000

    work

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,7 @@
 const assertEquals = function(actual, expected) {
   if (actual === expected) {
-    return console.log('✅✅✅Assertion Passed: ' + actual + ' === ' + expected);
-  } else if (actual != expected) {
-    return console.log('❌❌❌Assertion Failed: ' + actual + ' !== ' + expected);
+    return console.log(`✅✅✅Assertion Passed:${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    return console.log(`❌❌❌Assertion Failed:${actual} !== ${expected}`);
   }
 };
