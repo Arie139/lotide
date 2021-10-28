19ecd06df71ec40e6cf542952dee31e5df59bef0
Ariel <see-har@hotmail.com>
2021-10-28 04:10:32 +0000

    udated

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅✅✅Assertion Passed:${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    return console.log(`❌❌❌Assertion Failed:${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(someArray1,someArray2) {
+  for (let i = 0; i < someArray1.length; i++){
+    for (let j = 0; j < someArray2.length; j++){
+      if (someArray1[i] === someArray2[j]){
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+};
+
+const assertArraysEqual = function(someArray1,someArray2) {
+  return console.log(assertEqual(eqArrays(someArray1, someArray2), true))
+};
