38e024ca6f3c7fbc7396526c0fbb37903de605c6
Ariel <see-har@hotmail.com>
2021-10-31 21:20:03 +0000

    hi

--- findKey.js
+++ findKey.js
@@ -6,3 +6,20 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const findKey = function(obj,callback) {
+    const keyVal = Object.keys(obj);
+    for (key of keyVal){
+      if (callback(obj[key])) {
+        return key;
+      }
+    }
+};
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2))
